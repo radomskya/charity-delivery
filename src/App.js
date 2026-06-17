3a44bb50d12098f4bb31579f6a245005925e3c74
radomskya <avner@radomsky.co.uk>
2026-06-17 07:56:39 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -185,6 +185,7 @@ export default function CharityDeliverySystem() {
       pollMessage,
       deliveryMessage,
       butcherEmailTemplate,
+      butcherEmailAddress: butcherEmailAddress || null,
       cutoffDay,
       cutoffHour,
       cutoffMinute,
